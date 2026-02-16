7e0b945aa875fee9067393610bb35bdcc6125a7a
karthik <karthik042412@gmail.com>
2026-02-16 10:49:23 +0530

    Add YOLO model and camera configuration

--- camera_supervisor_constants.py
+++ camera_supervisor_constants.py
@@ -4,13 +4,13 @@
 CAMERA_CONFIG_PATH = "data/test_camera_config.yml"
 
 # Server Details
-SERVER_CAMERAMANAGERAPI_URL = "https://robotmanagergs.leapmile.com/cameramanager"
-TOKEN_ENDPOINT = "/auth/generate_new_token"
+SERVER_CAMERAMANAGERAPI_URL = ""
+TOKEN_ENDPOINT = ""
 CAMERA_TOPIC = "CAMERA_EVENTS"
-S3_URL = "https://robotmanagergsbucket.blr1.cdn.digitaloceanspaces.com/"
+S3_URL = ""
 
 # Message Broker
-MSGBROKER_HOST = "robotmanagergs.leapmile.com"
-MSGBROKER_PORT = "1883"
-MSGBROKER_USERNAME = "leapmile_user"
-MSGBROKER_PASSWORD = "leapmile1234"
+MSGBROKER_HOST = ""
+MSGBROKER_PORT = ""
+MSGBROKER_USERNAME = ""
+MSGBROKER_PASSWORD = ""
